0a789f52762e6fb27cd0a8e446670925ca137476
Nimrachudhary <106138592+Nimrachudhary@users.noreply.github.com>
2022-06-22 23:25:06 -0700

    sup

--- resources/views/admin/Contacts/Suppliers/list_Suppliers.blade.php
+++ resources/views/admin/Contacts/Suppliers/list_Suppliers.blade.php
@@ -26,11 +26,11 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="box-body">
-                                    <div class="row">
+                                    {{-- <div class="row">
                                         <div class="col-md-3">
                                             <input class="form-check-input" type="checkbox" id="inlineCheckbox1"
                                                 value="option1" />
-                                            <label class="form-check-label" for="inlineCheckbox1">Purchase Due</label>
+                                            <label class="form-check-label" for="inlineCheckbox1">Purchase Due</label></div></div> --}}
             <div class="row">
                 <div class="col-12 grid-margin stretch-card">
                     <div class="card">
@@ -60,77 +60,25 @@
                                                                         value="option1" />
                                                                     <label class="form-check-label" for="inlineCheckbox1">Purchase Due</label>
                                                                 </div>
-
                                                                 <div class="col-md-3">
                                                                     <input class="form-check-input" type="checkbox" id="inlineCheckbox2"
                                                                         value="option2" />
                                                                     <label class="form-check-label" for="inlineCheckbox2">Purchase Return</label>
-
                                                                 </div>
                                                                 <div class="col-md-3">
                                                                     <input class="form-check-input" type="checkbox" id="inlineCheckbox2"
                                                                         value="option2" />
                                                                     <label class="form-check-label" for="inlineCheckbox2">Advance Balance</label>
-
                                                                 </div>
                                                                 <div class="col-md-3">
                                                                     <input class="form-check-input" type="checkbox" id="inlineCheckbox2"
                                                                         value="option2" />
                                                                     <label class="form-check-label" for="inlineCheckbox2"> Opening Balance</label>
-
                                                                 </div>
                                                             </div>
-                                                            <br>
-                                                            <div class="row">
-                                                                <div class="col-md-4">
-                                                                    <div class="form-group">
-                                                                        <label for="Status">Status</label>
-                                                                        <i class="fa fa-info-circle text-info hover-q no-print " aria-hidden="true"
-                                                                            data-container="body" data-toggle="popover" data-placement="auto bottom"
-                                                                            data-content="Check/Uncheck to make a user active/inactive."
-                                                                            data-html="true" data-trigger="hover"></i>
-                                                                        <select name="Suppliers" id="" class="form-control">
-                                                                            <option selected value="Admin">None</option>
-                                                                            <option value="Admin">Active</option>
-                                                                            <option value="Cashier">Not Active</option>
-                                                                        </select>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-
-
-
-                                                        </div>
-
-
-
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </form>
-
-                                        <div class="col-md-3">
-                                            <input class="form-check-input" type="checkbox" id="inlineCheckbox2"
-                                                value="option2" />
-                                            <label class="form-check-label" for="inlineCheckbox2">Purchase Return</label>
-
-                                        </div>
-                                        <div class="col-md-3">
-                                            <input class="form-check-input" type="checkbox" id="inlineCheckbox2"
-                                                value="option2" />
-                                            <label class="form-check-label" for="inlineCheckbox2">Advance Balance</label>
-
-                                        </div>
-                                        <div class="col-md-3">
-                                            <input class="form-check-input" type="checkbox" id="inlineCheckbox2"
-                                                value="option2" />
-                                            <label class="form-check-label" for="inlineCheckbox2"> Opening Balance</label>
-
-                                        </div>
-                                    </div>
-                                    <br>
-                                    <div class="row">
+<br><br>
+                                                            {{-- lets --}}
+                                                              <div class="row">
                                         <div class="col-md-4">
                                             <div class="form-group">
                                                 <label for="Status">Status</label>
@@ -147,6 +95,61 @@
                                             </div>
                                         </div>
                                     </div>
+                                                            {{-- letsend --}}
+                                                            <br>
+
+
+
+                                                        </div>
+
+                                                    </div>
+
+                                                    </div>
+
+                                            </div>
+                                        </div>
+
+                                    </form>
+                    </div>
+                    {{-- check --}}
+
+                                        {{-- <div class="col-md-3">
+                                            <input class="form-check-input" type="checkbox" id="inlineCheckbox2"
+                                                value="option2" />
+                                            <label class="form-check-label" for="inlineCheckbox2">Purchase Return</label>
+
+                                        </div> --}}
+                                        {{-- <div class="col-md-3">
+                                            <input class="form-check-input" type="checkbox" id="inlineCheckbox2"
+                                                value="option2" />
+                                            <label class="form-check-label" for="inlineCheckbox2">Advance Balance</label>
+
+                                        </div>
+                                        <div class="col-md-3">
+                                            <input class="form-check-input" type="checkbox" id="inlineCheckbox2"
+                                                value="option2" />
+                                            <label class="form-check-label" for="inlineCheckbox2"> Opening Balance</label>
+
+                                        </div>
+                                    </div> --}}
+                                    <br>
+                                    {{-- <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label for="Status">Status</label>
+                                                <i class="fa fa-info-circle text-info hover-q no-print " aria-hidden="true"
+                                                    data-container="body" data-toggle="popover" data-placement="auto bottom"
+                                                    data-content="Check/Uncheck to make a user active/inactive."
+                                                    data-html="true" data-trigger="hover"></i>
+                                                <select name="Suppliers" id="" class="form-control">
+                                                    <option selected value="Admin">None</option>
+                                                    <option value="Admin">Active</option>
+                                                    <option value="Cashier">Not Active</option>
+
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div> --}}
 
 
 
@@ -160,7 +163,7 @@
                 </div>
             </form>
                                     {{-- try end --}}
-                    </div></div></div>
+                    </div></div>
 
             {{-- end --}}
 
